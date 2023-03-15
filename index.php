c8ac3d723ddf7671b1d49a095750d783893b501d
JeremieG06300 <jeremie.garod@epitech.eu>
2023-03-15 13:39:48 +0100

    updates index.php

--- index.php
+++ index.php
@@ -32,18 +32,32 @@
   <form method="POST">
     <link rel="stylesheet" type="text/css" href="index.css">
     <h2>Annonce Voiture</h2>
+    <label class="insctruc">Votre prix de départ :</label>
+    <br/>
     <input name="prix d" type="texte" placeholder="prix de depart">
     </input>
+    <label class="insctruc">Date de fin de votre enchère :</label>
+    <br/>
     <input name="date de fin" type="texte" placeholder="date de fin">
     </input>
+    <label class="insctruc">Modèle de votre véhicule :</label>
+    <br/>
     <input name="modele" type="texte" placeholder="modele">
     </input>
+    <label class="insctruc">Marque de votre véhicule :</label>
+    <br/>
     <input name="marque" type="texte" placeholder="marque">
     </input>
+    <label class="insctruc">Puissance de votre véhicule :</label>
+    <br/>
     <input name="puissance" type="texte" placeholder="puissance">
     </input>
+    <label class="insctruc">Année de votre véhicule :</label>
+    <br/>
     <input name="année" type="texte" placeholder="année">
     </input>
+    <label class="insctruc">Description de votre véhicule :</label>
+    <br/>
     <input name="description" type="texte" placeholder="description">
     </input>
     <button class="buttonSub"> valider </button>
