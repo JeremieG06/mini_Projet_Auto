9f367b1b8c2078e5b538ed686b7f27b7a45b0bed
JeremieG06300 <jeremie.garod@epitech.eu>
2023-03-15 16:23:30 +0100

    updates user.class.php

--- user.class.php
+++ user.class.php
@@ -4,7 +4,7 @@ private string $lastname;
 private string $firstname;
 private string $email;
 private string $password;
-}
+
 
 public function __construct( $lastname,$firstname,$email,$password)
 {
@@ -12,7 +12,10 @@ public function __construct( $lastname,$firstname,$email,$password)
    $this->firstname = $firstname;
    $this-> email = $email;
    $this-> password = $password;
+
+
 }
+
 public function getlastname() {
  return   $this->lastname;
 }
@@ -26,4 +29,27 @@ return   $this->email;
 }
 public function getpassword() {
     return  $this->password;
-    }
+    }    
+
+
+    public function setlastname($lastname) 
+    {
+      return   $this->lastname = $lastname;
+     }
+     
+     public function setfirstname($firstname) 
+     {
+     return   $this->firstname = $firstname;
+     }
+     
+     public function setemail($email) 
+     {
+     return   $this->email = $email;
+     }
+
+     public function setpassword($password)
+      {
+         return  $this->password = $password;
+      }    
+
+   }   
