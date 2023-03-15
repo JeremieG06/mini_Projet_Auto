e05407aaa62be1dd8fbfc4d37fcdc70a3ecac1d3
JeremieG06300 <jeremie.garod@epitech.eu>
2023-03-15 16:26:53 +0100

    updates user.class.php

--- user.class.php
+++ user.class.php
@@ -1,55 +1,60 @@
 <?php
-class user {
-private string $lastname;
-private string $firstname;
-private string $email;
-private string $password;
-
-
-public function __construct( $lastname,$firstname,$email,$password)
+class user
 {
-   $this->lastname = $lastname;
-   $this->firstname = $firstname;
-   $this-> email = $email;
-   $this-> password = $password;
+   private string $lastname;
+   private string $firstname;
+   private string $email;
+   private string $password;
 
+
+   public function __construct($lastname, $firstname, $email, $password)
+   {
+      $this->lastname = $lastname;
+      $this->firstname = $firstname;
+      $this->email = $email;
+      $this->password = $password;
+
+
+   }
+
+   public function getlastname()
+   {
+      return $this->lastname;
+   }
+
+   public function getfirstname()
+   {
+      return $this->firstname;
+   }
+
+   public function getemail()
+   {
+      return $this->email;
+   }
+   public function getpassword()
+   {
+      return $this->password;
+   }
+
+
+   public function setlastname($lastname)
+   {
+      return $this->lastname = $lastname;
+   }
+
+   public function setfirstname($firstname)
+   {
+      return $this->firstname = $firstname;
+   }
+
+   public function setemail($email)
+   {
+      return $this->email = $email;
+   }
+
+   public function setpassword($password)
+   {
+      return $this->password = $password;
+   }
 
 }
-
-public function getlastname() {
- return   $this->lastname;
-}
-
-public function getfirstname() {
-return   $this->firstname;
-}
-
-public function getemail() {
-return   $this->email;
-}
-public function getpassword() {
-    return  $this->password;
-    }    
-
-
-    public function setlastname($lastname) 
-    {
-      return   $this->lastname = $lastname;
-     }
-     
-     public function setfirstname($firstname) 
-     {
-     return   $this->firstname = $firstname;
-     }
-     
-     public function setemail($email) 
-     {
-     return   $this->email = $email;
-     }
-
-     public function setpassword($password)
-      {
-         return  $this->password = $password;
-      }    
-
-   }   
