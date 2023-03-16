10df30f70b122381157c5a1cfd2c4c06456a85db
PAROLDI-ALICE <aliceparoldi@gmail.com>
2023-03-16 09:42:18 +0100

    Udpate Post & Index

--- post.php
+++ post.php
@@ -1,17 +1,27 @@
 <?php
 require_once(__DIR__ . '/voiture.class.php');
-require_once(__DIR__ . '/user.class.php');
+require_once(__DIR__ . '/users.class.php');
+require_once(__DIR__ . '/index.php');
 
-// PDO
+
+// PDO DATABASE
 $dbh = new PDO("mysql:dbname=mini_projet_auto_enchere;host=127.0.0.1", "root", "");
-// Lecture de la réponse
-$result = $dbh->query("SELECT * FROM users");
 
-// Requête
+// Lecture de la réponse pour USERS
+$result = $dbh->query("SELECT * FROM users");
+// Requête pour USERS
 $query = $dbh->prepare('SELECT * FROM users WHERE email = :email');
 $query->bindValue(':email', 'pierre.lefevre@gmail.com', PDO::PARAM_STR); // Utilisation de PDO::PARAM_STR
 $query->execute(); // Execution de la requête préparée
 $results = $query->fetchAll();
+//Query pour ajout d'un USER connexion avec la database
+$query = $dbh->prepare("INSERT INTO users (lastname, firstname, email, password) VALUES (:lastname, :firstname, :email, :password)");
+$image->bindParam(":lastname", $lastname);
+$query->bindParam(":firstname", $firstname);
+$query->bindParam(":email", $email);
+$query->bindParam(":password", $password);
+$query->execute();
+
 
 // POST sur formulaire Annonce
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -27,4 +37,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $_POST["description"]
     );
     var_dump($voiture);
+
+    //Query pour ajout d'una annonce voiture connexion avec la database
+    $query = $dbh->prepare("INSERT INTO ads (image, prixDepart, dateFin, modele, marque, puissance, année, description) VALUES (:image, :prixDepart, :dateFin, :modele, :marque, :puissance, :année, :description)");
+    $image->bindParam(":image", $image);
+    $query->bindParam(":prixDepart", $prixDepart);
+    $query->bindParam(":dateFin", $dateFin);
+    $query->bindParam(":modele", $modele);
+    $query->bindParam(":marque", $marque);
+    $query->bindParam(":puissance", $puissance);
+    $query->bindParam(":année", $année);
+    $query->bindParam(":description", $description);
+    $query->execute();
+
+    echo "Annonce ajouté avec succès !";
 }
