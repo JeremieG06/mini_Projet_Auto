25b7eca94d0588f4be9eadda091ed2e1ce73833b
JeremieG06300 <jeremie.garod@epitech.eu>
2023-03-16 13:39:57 +0100

    updates

--- annonces.php
+++ annonces.php
@@ -1,4 +1,14 @@
 <?php
-
+ if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    echo "<h2>Récapitulatif de l'annonce :</h2>";
+    echo "<p>Image : " . $_POST["image"] . "</p>";
+    echo "<p>Prix de départ : " . $_POST["prixDepart"] . "</p>";
+    echo "<p>Date de fin : " . $_POST["dateFin"] . "</p>";
+    echo "<p>Modèle : " . $_POST["modele"] . "</p>";
+    echo "<p>Marque : " . $_POST["marque"] . "</p>";
+    echo "<p>Puissance : " . $_POST["puissance"] . "</p>";
+    echo "<p>Année : " . $_POST["annee"] . "</p>";
+    echo "<p>Description : " . $_POST["description"] . "</p>";
+}
 
 ?>
--- post.php
+++ post.php
@@ -3,11 +3,7 @@ require_once(__DIR__ . '/voiture.class.php');
 require_once(__DIR__ . '/users.class.php');
 require_once(__DIR__ . '/index.php');
 
-
 // PDO DATABASE
-
-
-
 try {
     $dbh = new PDO("mysql:dbname=mini_projet_auto_enchere;host=127.0.0.1", "root", "");
     foreach ($dbh->query('SELECT * from ads') as $row) {
@@ -18,7 +14,6 @@ try {
     die();
 }
 
-
 // Lecture de la réponse pour USERS
 $result = $dbh->query("SELECT * FROM users");
 // Requête pour USERS
@@ -27,7 +22,7 @@ $query->bindValue(':email', 'pierre.lefevre@gmail.com', PDO::PARAM_STR); // Util
 $query->execute(); // Execution de la requête préparée
 $results = $query->fetchAll();
 
-//Query pour ajout d'un USER connexion avec la database
+// Query pour ajout d'un USER connexion avec la database
 $query = $dbh->prepare("INSERT INTO users (lastname, firstname, email, password) VALUES (:lastname, :firstname, :email, :password)");
 $query->bindParam(":lastname", $lastname);
 $query->bindParam(":firstname", $firstname);
@@ -47,19 +42,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $_POST["annee"],
         $_POST["description"]
     );
-    var_dump($voiture);
 
     //Query pour ajout d'une annonce voiture - connexion avec la database
-    $query = $dbh->prepare("INSERT INTO ads (image, prixDepart, dateFin, modele, marque, puissance, annee, description) VALUES ('image', 'prixDepart', 'dateFin', 'modele', 'marque', 'puissance', 'annee', 'description')");
-    $query->bindParam(":image", $image);
-    $query->bindParam(":prixDepart", $prixDepart);
-    $query->bindParam(":dateFin", $dateFin);
-    $query->bindParam(":modele", $modele);
-    $query->bindParam(":marque", $marque);
-    $query->bindParam(":puissance", $puissance);
-    $query->bindParam(":annee", $annee);
-    $query->bindParam(":description", $description);
-    $query->execute();
+    $query = $dbh->prepare("INSERT INTO ads (image, prixDepart, dateFin, modele, marque, puissance, annee, description) 
+                            VALUES (:image, :prixDepart, :dateFin, :modele, :marque, :puissance, :annee, :description)");
+    $query->bindParam(":image", $_POST["image"]);
+    $query->bindParam(":prixDepart", $_POST["prixDepart"]);
+    $query->bindParam(":dateFin", $_POST["dateFin"]);
+    $query->bindParam(":modele", $_POST["modele"]);
+    $query->bindParam(":marque", $_POST["marque"]);
+    $query->bindParam(":puissance", $_POST["puissance"]);
+    $query->bindParam(":annee", $_POST["annee"]);
+    $query->bindParam(":description", $_POST["description"]);
 
-    echo "Annonce ajouté avec succès !";
+    
+   
+
 }
