c2684408300445745834db4c358ebbce853f6abc
JeremieG06300 <jeremie.garod@epitech.eu>
2023-03-20 09:45:00 +0100

    updates annonceDetail.php

--- annonceDetails.php
+++ annonceDetails.php
@@ -1,3 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/d4ade387aa.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>Mon compte</title>
+</head>
+
+<body>
+    <header>
+        <img src="./Images/logo.png" alt="voiture" />
+
+
+        <div class="title">
+            <h1>Vectus Enchères</h1>
+        </div>
+
+    </header>
+    <nav>
+        <div class="topnav">
+            <a class="active" href="index.php"><i class="fa-solid fa-house-chimney"></i></a>
+            <a href="annonces.php">Annonces</a>
+            <a class="compte" href="compte.php">Mon Compte</a>
+        </div>
+    </nav>
+
+
+
 <?php
 require_once(__DIR__ . '/fonctionAnnonceDetails.php');
 
@@ -8,3 +40,7 @@ if ($id !== null) {
     echo "L'identifiant de l'annonce n'a pas été spécifié.";
 }
 ?>
+
+</body>
+
+</html>
