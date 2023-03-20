ee4a4f04c10aae153a86f2ada7aa067f68ae6c17
PAROLDI-ALICE <aliceparoldi@gmail.com>
2023-03-20 11:50:36 +0100

    Updates Post Login users

--- post.login.users.php
+++ post.login.users.php
@@ -1,22 +1,34 @@
 <?php
+//Démarrage de session:
+session_start();
+//Début de condition de login
 
-
-
-if (isset($_POST['email']) &&  isset($_POST['password'])) {
-    foreach ($users as $users) {
-        if (
-            $users['email'] === $_POST['email'] &&
-            $users['password'] === $_POST['password']
-        ) {
-            $loggedUser = [
-                'email' => $users['email'],
-            ];
-        } else {
-            $errorMessage = sprintf('Les informations envoyées ne permettent pas de vous identifier : (%s/%s)',
-                $_POST['email'],
-                $_POST['password']
-            );
+//PDO DB
+try {
+    $dbh = new PDO("mysql:dbname=mini_projet_auto_enchere;host=127.0.0.1", "root", "");
+    foreach ($dbh->query('SELECT * from users') as $row) {
+        // print_r($row);
+        if (isset($_POST['email']) &&  isset($_POST['password'])) {
+            foreach ($users as $users) {
+                if (
+                    $users['email'] === $_POST['email'] &&
+                    $users['password'] === $_POST['password']
+                ) {
+                    $loggedUser = [
+                        'email' => $users['email'],
+                    ];
+                } else {
+                    $errorMessage = sprintf(
+                        'Les informations envoyées ne permettent pas de vous identifier : (%s/%s)',
+                        $_POST['email'],
+                        $_POST['password']
+                    );
+                }
+            }
         }
     }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
 }
-?>
